2fcecb80f9f5041523930b347555518ab82f2510
Bobby McGill <bobby.n.mcgill@nasa.gov>
2018-11-28 13:30:54 -0600

    Completed Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  timeClicks=[];
+  numClick=0;
+  paraVis=true;
+  now = new Date();
+
+  doStuff(){
+    this.numClick++;
+    this.now = new Date();
+    this.timeClicks.push(this.now);
+    
+
+    if (this.paraVis){
+      this.paraVis = false;
+    } else {
+      this.paraVis = true;
+    }
+  }
+
+
+  getColor(){
+    if (this.numClick >= 5){
+      return "lightblue";
+    } else {
+      return "white";
+    }
+  }
+
 }
+
+
